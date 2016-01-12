e9ef17cf9d567676c3409bd053099f0d482e727a
aliouswang <wsw622@126.com>
2016-01-12 16:32:00 +0800

    add library module

--- app/src/main/java/com/aliouswang/http/sprite/MainActivity.java
+++ app/src/main/java/com/aliouswang/http/sprite/MainActivity.java
@@ -1,8 +1,18 @@
 package com.aliouswang.http.sprite;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +23,44 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                try {
+                    doHttpReqeust();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+    }
+
+    public void doHttpReqeust() throws Exception{
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://test.api.51jiabo.com:1080/hxjb/decoration/case/v1.0/list.do")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                }
+
+                Log.e("sprite", response.body().string());
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/aliouswang/http/sprite/api/ApiService.java
@@ -0,0 +1,10 @@
+package com.aliouswang.http.sprite.api;
+
+/**
+ * Created by aliouswang on 16/1/12.
+ */
+public interface ApiService {
+
+    
+
+}
